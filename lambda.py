e56df7ac804d38f65774af8314696bdede14287f
Suneel Kumar <suneelkumar@Suneels-MacBook-Air.local>
2025-07-30 13:05:28 +0530

    Updated Lambda Code

--- lambda.py
+++ lambda.py
@@ -1,6 +1,15 @@
+import json
+
 def lambda_handler(event, context):
     name = event.get("queryStringParameters", {}).get("name", "Guest")
+    message = f"Hello, {name}! Welcome to the API Gateway demo!"
+    
     return {
         "statusCode": 200,
-        "body": f"Hello, {name}! Welcome to the API Gateway demo!"
+        "headers": {
+            "Access-Control-Allow-Origin": "*",     # Allow all origins (CORS)
+            "Access-Control-Allow-Methods": "GET", # Allowed methods
+            "Access-Control-Allow-Headers": "*"    # Allowed headers
+        },
+        "body": json.dumps({"message": message})  # Return JSON string
     }
